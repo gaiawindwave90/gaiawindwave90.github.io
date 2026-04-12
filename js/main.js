be2939cbe83b2a36df8c0d78b864abb1d118535f
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-12 18:31:16 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -36,6 +36,13 @@ function textSwap() {
 }
 
   document.getElementById("year").innerHTML = new Date().getFullYear();
+ 
+
+ //April Fools shit.
+  var today = new Date();
+if (today.getFullYear() === 2027 && today.getMonth() === 3 && today.getDate() === 1) {
+    window.location.href = "https://versolot.github.io/";
+}
   
   
   	var msg = new Array();
